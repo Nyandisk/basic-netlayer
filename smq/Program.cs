b85dcf8f955b5df9d3ea37f8fc2b3a18cde4585f
ndisk <87699355+Nyandisk@users.noreply.github.com>
2025-04-22 00:17:54 +0300

    ReadLine for console output

--- smq/Program.cs
+++ smq/Program.cs
@@ -6,15 +6,19 @@ namespace smq{
         public static bool IsServerInstance { get; private set; } = false;
         public static bool IsClientInstance { get; private set; } = false;
         public static void Main(string[] args){
-            if (Console.ReadLine()! == "server") {
-                Console.Title = "Server";
-                IsServerInstance = true;
-                NetServer server = new(7070, 10);
-                server.Start();
-            } else {
-                Console.Title = "Client";
-                IsClientInstance = true;
-                _ = new NetClient(Console.ReadLine()!,"127.0.0.1", 7070);
+            try {
+                if (Console.ReadLine()! == "server") {
+                    Console.Title = "Server";
+                    IsServerInstance = true;
+                    NetServer server = new(7070, 1);
+                    server.Start();
+                } else {
+                    Console.Title = "Client";
+                    IsClientInstance = true;
+                    _ = new NetClient(Console.ReadLine()!, "127.0.0.1", 7070);
+                }
+            } finally {
+                Console.ReadLine();
             }
         }
     }
