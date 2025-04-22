66fcec62cde5bcb7a61dac0847c67b6d63c6568a
ndisk <87699355+Nyandisk@users.noreply.github.com>
2025-04-22 18:37:29 +0300

    more log options

--- smq/Networking/Log.cs
+++ smq/Networking/Log.cs
@@ -1,18 +1,22 @@
 ﻿namespace Vikinet2.Networking {
     public static class Log {
         public static bool WriteDebug { get; set; } = true;
+        public static bool WriteInfo { get; set; } = true;
+        public static bool WriteError { get; set; } = true;
         public static void Write(object? msg) {
+            if (!WriteInfo) return;
             Console.WriteLine($"{DateTime.Now:HH:mm:ss:fff} {msg}");
         }
         public static void Debug(object? msg) {
             if (!WriteDebug) return;
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Write(msg);
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss:fff} {msg}");
             Console.ResetColor();
         }
         public static void Error(object? msg) {
+            if (!WriteError) return;
             Console.ForegroundColor = ConsoleColor.Red;
-            Write(msg);
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss:fff} {msg}");
             Console.ResetColor();
         }
     }
